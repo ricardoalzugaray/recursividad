b281fafcab52edd1c67a7078055b5d3a5e7e7e0f
ricardoalzugaray <131892627+ricardoalzugaray@users.noreply.github.com>
2024-05-28 21:24:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace GenerarNumerosPrimos
+{
+    class Program
+    {
+        #region Verificar  Primo
+        static bool EsPrimo(int num)
+        {
+            return !EsDivisible(num, num - 1);
+        }
+        #endregion Verificar Primo
+        static bool EsDivisible(int num, int D)
+        {
+            if (D > 2)
+            {
+                return num % D == 0 || EsDivisible(num, D - 1);
+            }
+            else
+            {
+                return num % 2 == 0;
+
+            }
+        }
+            static void Main(string[] args)
+            {
+                int desdeNum, hastaNum;
+                bool resultado;
+
+                Console.WriteLine("Ingrese el intervalo");
+                desdeNum = Convert.ToInt32(Console.ReadLine());
+                hastaNum = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\n Los números generados desde {0} a {1} son:", desdeNum, hastaNum);
+
+                for (int num = desdeNum; num <= hastaNum; num++)
+                {
+                    resultado = EsPrimo(num);
+
+                    if (resultado)
+                    {
+                        Console.WriteLine(num);
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+
